8f13c5eeb85788cb65f939cc9cee6dcfc62eed53
Julian <sahaguj@pnw.edu>
2023-12-14 23:31:29 -0600

    create trebuchet.rs

---
+++ Day1/trebuchet.rs
@@ -0,0 +1,26 @@
+//Day 1 Advent of Code 2023
+//Author: bluju
+
+/*resources
+https://doc.rust-lang.org/book/ch12-02-reading-a-file.html
+*/
+
+fn main() {
+    //first digit value and last digit value found in string combined form a two digit number
+    //add all two digit numbers together for final input 
+    let mut first: char = '';
+    let mut last: char = '';
+    let mut total: i32 = 0;
+
+    //read a string from the file
+    //starting from the left of the string, find the first digit
+    //starting from the right of the string, find the last digit
+    //combine the two digits into a two digit number string
+    //convert the string into an integer
+    //add the integer to the total
+    //repeat until the end of the file is reached
+    //print the total
+
+
+
+}
